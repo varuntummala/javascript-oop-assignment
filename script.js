cdf782f454044ca31157673a63f12308e33a5c65
Varun Tummala <varunt@gmail.com>
2023-06-27 20:49:33 -0400

    Final Submission

--- script.js
+++ script.js
@@ -7,3 +7,33 @@
 // (This is called a 'getter' in Object Oriented Programming)
 
 // If you're stuck, take a look at the example class here for reference: https://www.geeksforgeeks.org/introduction-object-oriented-programming-javascript/#
+
+
+class Car {
+    constructor(make,model,year,color) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+    // methods
+    getMake() {
+        return (`${this.make}`);
+    }
+
+    getModel() {
+        return (`${this.model}`);
+    }
+
+    getYear() {
+        return (`${this.year}`);
+    }
+
+    getColor() {
+        return (`${this.color}`);
+    }
+}
+
+let car1 = new Car(`Nissan`,`Pathfinder`,2015,`White`);
+
+console.log(car1);
